8425401ad4604cfd13221ccba2c678a58dd66e76
Ryan <ryanjones0911@gmail.com>
2026-05-20 00:27:46 -0500

    start build of Projects page, componentize project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='project-card'>
+    <h2 class='title'>{{ title }}</h2>
+    <img :src="src" :alt="altText">
+    <p>{{ description }}</p>
+    <a :href="url" target="_blank">Github</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+  src: string
+  altText: string
+  url: string
+  ghTitle: string
+}>()
+
+
+
+
+</script>
+
+
+
+<style scoped>
+.project-card {
+  padding: 1.5rem 1.6rem;
+  margin-bottom: 1.25rem;
+  border: 1px solid #e1e1e1;
+  border-radius: 1rem;
+  background-color: #f8f8f8;
+
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>This is the Projects page</h1>
+  <main>
+    <h1>Things I've Done</h1>
+    <ProjectCard title="Solar System Simulation" description="details forthcoming" src="/solarsystem.png"
+      altText="solar system" url="https://github.com/Ryanjones0911/UTD-Graphics-Final-Project" />
+
+  </main>
 </template>
+
+<script setup lang="ts">
+import ProjectCard from '@/components/ProjectCard.vue';
+</script>
+
+<style scoped>
+main {
+  display: grid;
+  place-content: center;
+  text-align: center;
+  min-height: 30vh;
+  max-width: 850px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 3rem;
+  gap: 1.5rem;
+}
+</style>
